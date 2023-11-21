fc9f4b0709b16d7ca476e97941fd0e0f9180f437
ivan <ivantrajanovski@gmail.com>
2023-11-21 14:23:03 +0100

    add react navigator

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import CategoriesScreen from "./screens/CategoriesScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
+
   return (
     <>
       <StatusBar style="auto" />
-      <CategoriesScreen />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Categories"
+            component={CategoriesScreen}
+            options={{ title: "All Categories" }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
