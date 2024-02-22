c6ee6733bb4606fa9f46d115932f32df728f581b
Ivan Trajanovski <ivantrajanovski@gmail.com>
2024-02-22 11:20:57 +0100

    show content in meal detail screen

--- components/MealItem.js
+++ components/MealItem.js
@@ -8,6 +8,7 @@ import {
   View,
 } from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import MealDetails from "./MealDetails";
 
 export default function MealItem({
   id,
@@ -37,13 +38,11 @@ export default function MealItem({
             <Image source={{ uri: imageUrl }} style={styles.image} />
             <Text style={styles.title}>{title}</Text>
           </View>
-          <View style={styles.details}>
-            <Text style={styles.detailItem}>{duration}m</Text>
-            <Text style={styles.detailItem}>{complexity.toUpperCase()}</Text>
-            <Text style={styles.detailItem}>
-              ${affordability.toUpperCase()}
-            </Text>
-          </View>
+          <MealDetails
+            duration={duration}
+            complexity={complexity}
+            affordability={affordability}
+          />
         </View>
       </Pressable>
     </View>
@@ -76,16 +75,7 @@ const styles = StyleSheet.create({
     width: "100%",
     height: 200,
   },
-  details: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-around",
-    padding: 8,
-  },
-  detailItem: {
-    marginHorizontal: 4,
-    fontSize: 12,
-  },
+
   buttonPressed: {
     opacity: 0.75,
   },
